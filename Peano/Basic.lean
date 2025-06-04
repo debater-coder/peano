347efcd45b8c8ac5ec8552386bf84a2a1e36fd64
debater-coder <hamzah@syedahmed.net>
2025-06-04 20:30:37 +1000

    more theorems

--- Peano/Basic.lean
+++ Peano/Basic.lean
@@ -1,3 +1,5 @@
+import Mathlib
+
 inductive MyNat where
   | zero : MyNat
   | succ : MyNat -> MyNat
@@ -18,13 +20,34 @@ def add (x y : MyNat) :=
 
 infixl:65 "+" => add
 
-theorem add_zero : x + zero = x := by rfl
+
+@[simp] theorem add_zero : x + zero = x := by rfl
 theorem add_succ : x + succ n = succ (x + n) := by rfl
 
 
-theorem zero_add : zero + x = x := by
+@[simp] theorem zero_add : zero + x = x := by
   induction x with
-  | zero => rw [add_zero]
+  | zero => rfl
   | succ d ih => rw [add_succ, ih]
+
+theorem succ_add : succ n + x = succ (n + x) := by
+  induction x with
+  | zero => rfl
+  | succ d ih => rw [add_succ, add_succ, ih]
+
+theorem succ_eq_add_one (n : MyNat): succ n = n + (1:MyNat) := by rfl
+
+theorem succ_inj (a b : MyNat) : succ a = succ b → a = b := by
+  sorry
+
+theorem add_right_cancel (a b n : MyNat) : a + n = b + n → a = b := by
+  intro h
+  induction n with
+  | zero => exact h
+  | succ d ih =>
+    repeat rw [add_succ] at h
+    apply succ_inj at h
+    exact ih h
+
 
 end MyNat
